3d9984424dcda59b5bb3d0332f95c85506ee30e1
Andrew Cline <acline@precisionplanting.com>
2024-07-31 09:13:46 -0500

    feat: a few updates to the app to make it nicer

--- src/main.ts
+++ src/main.ts
@@ -10,34 +10,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const DATABASE_NAME = process.env.DATABASE_NAME;
+const AWS_ENDPOINT = process.env.AWS_ENDPOINT;
 
-console.log(DATABASE_NAME);
-
-const dynamo = new DynamoDBClient({
-  endpoint: "http://localhost:4566",
-  region: "us-east-2",
-});
+const dynamo = new DynamoDBClient({ region: "us-east-2" });
+const ddb = DynamoDBDocument.from(dynamo);
 
 const app = new Hono();
 
 app.get("/up", (c) => c.json({ status: "up" }));
 
 app.post("/users", async (c) => {
+  await ddb.put({
+    TableName: DATABASE_NAME,
+    Item: { pk: ulid(), sk: "USER" },
+  });
   return c.json({ created: true }, 204);
 });
 
 app.get("/users", async (c) => {
-  const result = await dynamo.send(
-    new ScanCommand({ TableName: DATABASE_NAME }),
-  );
+  const result = await ddb.scan({ TableName: DATABASE_NAME });
 
-  return c.json(result);
+  return c.json({ items: result.Items });
 });
-
-export const handler = handle(app);
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   serve(app, ({ port }) => {
     console.log(`listening on port ${port}`);
   });
 }
+
+export const handler = handle(app);
